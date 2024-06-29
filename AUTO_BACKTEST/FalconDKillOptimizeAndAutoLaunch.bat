350e2b2d0f20a8ae7c820ac29a52454947f3eecc
Vladimir Zhbanko <vz.home.experiments@gmail.com>
2024-06-29 21:42:48 +0200

    commented call because we will run this from R

--- AUTO_BACKTEST/FalconDKillOptimizeAndAutoLaunch.bat
+++ AUTO_BACKTEST/FalconDKillOptimizeAndAutoLaunch.bat
@@ -84,9 +84,9 @@ rem *************************************************
 rem *** 3. Starts all terminals again after waiting 30 seconds ***
 rem *************************************************
 
-ping localhost -n 10
+::ping localhost -n 10
 
-call "%PATH_STUP%\MetaTraderAutoLaunch.cmd"
+::call "%PATH_STUP%\MetaTraderAutoLaunch.cmd"
 
 
 exit
